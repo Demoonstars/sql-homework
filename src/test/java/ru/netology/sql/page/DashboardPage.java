3a49ea1b42363c3a0990f99b06270ae3a7739174
Anton <Antonpopikhin@gmail.com>
2026-03-29 00:42:35 +0300

    Fix dashboard locator to use visible text

--- src/test/java/ru/netology/sql/page/DashboardPage.java
+++ src/test/java/ru/netology/sql/page/DashboardPage.java
@@ -3,10 +3,10 @@ package ru.netology.sql.page;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class DashboardPage {
-    private final SelenideElement heading = $("[data-test-id=dashboard]");
+    private final SelenideElement heading = $x("//h2[contains(text(), 'Личный кабинет')]");
 
     public DashboardPage() {
         heading.shouldBe(Condition.visible);
