9111224a4b0381be61a7a7ac35a78662e3bf5037
Anton <Antonpopikhin@gmail.com>
2026-03-28 21:30:27 +0300

    Final SQL homework

---
+++ README.md
@@ -0,0 +1,3 @@
+# Домашнее задание к занятию «SQL»
+
+[![Java CI with Gradle](https://github.com/Demoonstars/sql-homework/actions/workflows/gradle.yml/badge.svg)](https://github.com/Demoonstars/sql-homework/actions/workflows/gradle.yml)
---
+++ src/test/java/ru/netology/sql/SqlTest.java
@@ -0,0 +1,32 @@
+package ru.netology.sql;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.sql.data.DataHelper;
+import ru.netology.sql.data.SQLHelper;
+import ru.netology.sql.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class SqlTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @AfterAll
+    static void teardown() {
+        SQLHelper.cleanDatabase();
+    }
+
+    @Test
+    void shouldSuccessfullyLogin() {
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+}
